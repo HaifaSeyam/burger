f993aae0a7eaed8c3e48ad8f6c739c3d1cb05743
HaifaSeyam <h.abuamro@hotmail.com>
2019-03-14 00:09:30 -0500

    handlebars files created - css and frontend js files as well

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,7 @@
+//When submit button clicked we add new burger to the DB
+$("#submitBtn").on("click", function(event){
+    event.preventDefault();
+
+    var burgerName = $("#burgerName").val().trim();
+
+});
